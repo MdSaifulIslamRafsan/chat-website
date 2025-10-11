40b0b6af33465202e6a1fd3ddeee02ccad41cd60
MdSaifulIslamRafsan <mdsaifulislamrafsan2222222@gmail.com>
2025-10-11 16:52:03 +0600

    setup redux

--- chat-app-frontend/src/main.tsx
+++ chat-app-frontend/src/main.tsx
@@ -2,15 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-
 import { ThemeProvider } from "./components/theme-provider.tsx";
-
+import { Provider } from 'react-redux'
 import { router } from "./routes/routes.tsx";
+import { store } from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="theme">
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </ThemeProvider>
   </StrictMode>
 );
