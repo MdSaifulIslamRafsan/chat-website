2a44bca230e3d42dc50b0ea561e6b9ff5efc5689
MdSaifulIslamRafsan <mdsaifulislamrafsan2222222@gmail.com>
2025-10-22 16:39:21 +0600

    fix vercel spa page related issue solve

--- chat-app-frontend/src/redux/api/baseApi.ts
+++ chat-app-frontend/src/redux/api/baseApi.ts
@@ -15,7 +15,7 @@ interface MyErrorData {
   code: number;
 }
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api/v1",
+  baseUrl: import.meta.env.VITE_API_BASE_URL,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
@@ -37,10 +37,13 @@ const baseQueryWithRefreshToken: BaseQueryFn<
     toast.error((result.error.data as MyErrorData).message);
   }
   if (result.error?.status === 401) {
-    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
-      method: "POST",
-      credentials: "include",
-    });
+    const res = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/auth/refresh-token`,
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    );
     const data = await res.json();
     if (data?.data?.accessToken) {
       const user = (api.getState() as RootState).auth.user;
